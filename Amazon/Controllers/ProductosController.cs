5b2fdff6adf9b9b0417cfa84103ac7255401ceba
parra.diaz.maria.f@gmail.com <parra.diaz.maria.f@gmail.com>
2023-09-30 01:41:49 -0700

    Update

---
+++ Amazon/Controllers/ProductosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Amazon.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult Detalles(int id)
+        //{
+        //    var producto = ObtenerDetallesDelProducto(id);
+        //    return View(producto);
+        //}
+    }
+}
